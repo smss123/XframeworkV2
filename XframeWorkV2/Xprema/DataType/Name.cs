0b9bb6b7a1d8a1e65f7d69ec77d10b5e9bc52126
smss123 <smsssamer@live.com>
2014-09-29 00:04:52 +0400

    Create Name

---
+++ XframeWorkV2/Xprema/DataType/Name.cs
@@ -0,0 +1,38 @@
+﻿using Xprema;
+using Xprema.Helper;
+public  struct Name
+{
+    public string FirstName;
+    public string ScondName;
+    public string LastName;
+    public string  FullName 
+    {
+        get
+        {
+            return string.Format("{0} {1} {2}",FirstName,ScondName,LastName);
+        }
+
+        set
+        {
+            string[] names = value.Split(' ');
+            try
+            {
+                FirstName = names[0];
+                ScondName = names[1];
+                LastName = names[2];
+            }
+            catch (System.Exception e)
+            {
+
+                throw new XpremaException(ErrorMessageDictionary.InvalidName().Description,
+                       ErrorMessageDictionary.InvalidName().CodeNumber,
+                       ErrorMessageDictionary.InvalidName().OtherDescription,
+                      ErrorMessageDictionary.InvalidName().UserMessage,
+                        ErrorMessageDictionary.InvalidName().UserMessageArabic);
+            }
+        }
+    }
+        
+       
+
+}
