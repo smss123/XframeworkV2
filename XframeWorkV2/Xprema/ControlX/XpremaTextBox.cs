a4ec2469d7fa9697b50ab6606ecbf54aa536c7dd
smss123 <smsssamer@live.com>
2014-10-02 02:23:56 -0700

    Crate Text Boxx

---
+++ XframeWorkV2/Xprema/ControlX/XpremaTextBox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Xprema.ControlX
+{
+    public partial class XpremaTextBox : UserControl
+    {
+        public TextBox TextBoxSetting 
+        {
+
+            get
+            {
+                return textBox1;
+            }
+            set
+            {
+                textBox1 = value;
+            }
+        
+        }
+        public bool  AllowNull { get; set; }
+        string hideText;
+        public string  HideText {
+            get
+            {
+                return hideText;
+            } set
+              {
+              	hideText = value;
+              }
+        
+        }
+        public XpremaTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
