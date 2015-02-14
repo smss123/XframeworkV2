716c6003450cdc2d8bd1d64f65546152f36c4bc2
Abu Ehab <MyGlassBox@Hotmail.Com>
2015-02-14 15:22:29 +0200

    UpDate 1.0

---
+++ XframeWorkV2/Xprema/Athers/ChangeLanguage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Xprema.Athers
+{
+   public class ChangeLanguage
+    {
+        public static void SetKeyboardLanguage(string CultureName)
+        {
+            System.Globalization.CultureInfo MyLang = new System.Globalization.CultureInfo(CultureName);
+            InputLanguage InLang = InputLanguage.FromCulture(MyLang);
+            InputLanguage.CurrentInputLanguage = InLang;
+        }
+
+    }
+}
---
+++ XframeWorkV2/Xprema/Athers/TheScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Xprema.Athers
+{
+   public  class TheScreen
+    {
+        // كود معرفة دقة الشاشة Get Screen Resolution
+        public static string GetScreenResolution()
+        {
+            string ScrRes = Screen.PrimaryScreen.Bounds.Width + "X" + Screen.PrimaryScreen.Bounds.Height;
+            return ScrRes;
+        }
+    }
+}
